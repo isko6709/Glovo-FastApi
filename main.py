251c7aac16efa008b91b48c72ee6388161d9d354
isko6709 <isko6709@gmail.com>
2026-03-12 08:58:11 +0600

    dsds

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-
 import uvicorn
 from mysite.api import (
     user, category, store, contact, address,
@@ -9,19 +8,20 @@ from mysite.admin.setup import setup_admin
 
 glovo_app = FastAPI(title="glovo")
 
+glovo_app.include_router(user.user_router)
+glovo_app.include_router(category.category_router)
+glovo_app.include_router(store.store_router)
+glovo_app.include_router(contact.contact_router)
+glovo_app.include_router(address.address_router)
+glovo_app.include_router(store_menu.store_menu_router)
+glovo_app.include_router(product.product_router)
+glovo_app.include_router(order.order_router)
+glovo_app.include_router(courier_product.courier_router)
+glovo_app.include_router(review.review_router)
+glovo_app.include_router(auth.auth_router)
 
-app.include_router(user.user_router)
-app.include_router(category.category_router)
-app.include_router(store.store_router)
-app.include_router(contact.contact_router)
-app.include_router(address.address_router)
-app.include_router(store_menu.store_menu_router)
-app.include_router(product.product_router)
-app.include_router(order.order_router)
-app.include_router(courier_product.courier_router)
-app.include_router(review.review_router)
-app.include_router(auth.auth_router)
-setup_admin(app)
+setup_admin(glovo_app)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(glovo_app, host="127.0.0.1", port=8000)
+
